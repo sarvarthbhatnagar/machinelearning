5b35d24f200aca5ac54a96aa4630c1f8d22fa4d0
Sarvarth Bhatnagar <sarvarth.bhatnagar@ibm.com>
2018-11-11 09:50:14 -0500

    info changes

--- regression/simple_linear_regression.py
+++ regression/simple_linear_regression.py
@@ -1,34 +1,32 @@
 # Simple Linear Regression
 
-# Importing the libraries
-import numpy as np
+# pylot for chart plotting
 import matplotlib.pyplot as plt
+# pandas for file read
 import pandas as pd
 
-# Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
+# [columns,rows] (: = all) (-1 = all-1)
+# y = dependent variable. y = f(x)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
+# random_state is the seed number. it ensures common result on split for every run.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Simple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
+# regressor.fit( x_train, f(x)_train)
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+
+# while plotting scatter would give a scatter graph. If we use plot, that would give a linear graph on top of scatter.
 
 # Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
